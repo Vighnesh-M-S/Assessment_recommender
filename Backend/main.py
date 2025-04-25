8ea7d7d0fd18a152a5a73b9851fac3b0c620bc9d
VIGHNESH M S <princevighnesh71@gmail.com>
2025-04-26 02:23:42 +0530

    main update

--- Backend/main.py
+++ Backend/main.py
@@ -74,5 +74,5 @@ async def query_assessments(payload: Query):
     results = [format_row(df.iloc[idx]) for idx in I[0]]
     return {"query": payload.query, "recommendations": results}
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
