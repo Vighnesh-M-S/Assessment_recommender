17910dbdf5d29010372dfd2e02138bfa13a84087
VIGHNESH M S <princevighnesh71@gmail.com>
2025-04-26 04:55:24 +0530

    update

--- Backend/main.py
+++ Backend/main.py
@@ -75,4 +75,4 @@ async def query_assessments(payload: Query):
     return {"query": payload.query, "recommendations": results}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9090, reload=True)
